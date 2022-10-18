cd0ab9edf8fc72901d6372472444f31e7ca6421f
Manuel Herrera <m.herreralara80@gmail.com>
2022-10-18 00:37:20 -0500

    home para usuarios autenticados usando Mixin

--- apoyos/admin.py
+++ apoyos/admin.py
@@ -4,15 +4,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Apoyos, Departamento, EncargadoRuta, Localidad, Persona, Puesto, Empleado
 
-
 class LocalidadAdmin(admin.ModelAdmin):
     """ modelelo administrativo de Localidad"""
     ordering = ['id']
     list_display = ('id', 'nombre', 'seccion', 'numero_habitantes')
     list_filter = ['nombre', 'seccion']
     search_fields = ['id', 'nombre']
-    list_per_page = 10   
+    list_per_page = 10
     
+
 class DepartamentoAdmin(admin.ModelAdmin):
     """ modelo administrativo de Departamento"""
     ordering = ['id']
@@ -20,43 +20,52 @@ class DepartamentoAdmin(admin.ModelAdmin):
     list_filter = ['nombre']
     search_fields = ['id', 'nombre']
     list_per_page = 10
-    
+
+
 class EncargadoRutaAdmin(admin.ModelAdmin):
     """ modelo administrativo de EnccargadoRuta"""
     ordering = ['id']
-    list_display = ('id', 'nombres', 'ap_paterno', 'ap_materno', 'departamento', 'sexo', 'telefono', 'domicilio', 'email')
+    list_display = ('id', 'nombres', 'ap_paterno', 'ap_materno',
+                    'departamento', 'sexo', 'telefono', 'domicilio', 'email')
     list_filter = ['nombres', 'departamento', 'sexo']
     search_fields = ['id', 'nombres', 'ap_paterno']
     list_per_page = 8
-    
+
+
 class ApoyosAdmin(admin.ModelAdmin):
     """ modelo administrativo de Apoyos"""
     ordering = ['id']
-    list_display = ('id', 'tipo', 'descripcion', 'fecha_de_entrega', 'notas_adicionales', 'persona', 'encargado_de_ruta')
-    list_filter = ['persona', 'fecha_de_entrega', 'tipo', 'encargado_de_ruta']
+    list_display = ('id', 'tipo', 'descripcion', 'fecha_de_entrega',
+                    'notas_adicionales', 'persona', 'encargado_de_ruta')
+    list_filter = ['persona', 'encargado_de_ruta', 'fecha_de_entrega', 'tipo']
     search_fields = ['id', 'tipo', 'descripcion']
     list_per_page = 8
-    
+
+
 class PersonaAdmin(admin.ModelAdmin):
     """ modelo administrativo de Persona"""
     ordering = ['id']
-    list_display = ('id', 'nombres', 'ap_paterno', 'ap_materno', 'tipo', 'domicilio', 'estado', 'telefono', 'localidad')
-    list_filter = ['tipo', 'estado', 'localidad']
+    list_display = ('id', 'nombres', 'ap_paterno', 'ap_materno', 'tipo', 'genero', 'estado', 'telefono', 'localidad')
+    list_filter = ['tipo', 'localidad', 'estado']
     search_fields = ['id', 'nombres', 'ap_paterno']
     list_per_page = 12
-    
+
+
 class PuestoAdmin(admin.ModelAdmin):
     """ modelo admin de puesto"""
     ordering = ['id']
     list_display = ('id', 'nombre')
     search_fields = ['nombre']
     list_per_page = 12
-    
+
+
 class EmpleadoAdmin(admin.ModelAdmin):
     """ modelo admin de empleado"""
     ordering = ['id']
-    list_display = ('id', 'nombre', 'ap_paterno', 'ap_materno', 'sexo', 'departamento', 'puesto', 'status', 'telefono')
-    list_filter = ('departamento', 'puesto', 'status', 'sexo')
+    list_display = ('id', 'nombre', 'ap_paterno', 'ap_materno',
+                    'genero', 'departamento', 'puesto', 'status', 'telefono')
+    list_filter = ('departamento', 'puesto', 'status', 'genero')
+
 
 admin.site.register(Localidad, LocalidadAdmin)
 admin.site.register(Departamento, DepartamentoAdmin)
--- apoyos/models.py
+++ apoyos/models.py
@@ -1,5 +1,4 @@
 
-from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
@@ -30,6 +29,24 @@ class Localidad(models.Model):
         """ nombre en plural(Muchos) del modelo"""
         verbose_name_plural = "Localidades"
 
+class Puesto(models.Model):
+    """ modelo de Puesto de trabajo"""
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        """ descripcion del puesto"""
+        return '{}'.format(self.nombre)
+
+    def save(self):
+        """ el nombre del puesto de trabajo lo guardamos en mayúsculas"""
+        self.nombre = self.nombre.upper()
+        super(Puesto, self).save()
+
+    class Meta:
+        """ nombre en plural(Muchos) del modelo"""
+        verbose_name_plural = "Puestos"
+
+
 class Persona(models.Model):
     """ modelo persona"""
     nombres = models.CharField(
@@ -44,16 +61,16 @@ class Persona(models.Model):
 
     ap_materno = models.CharField(
         max_length=100,
-        null = True,
-        blank = True
+        null=True,
+        blank=True
     )
 
-    #example of choices
+    # example of choices
     ACTIVISTA = 'ACTIVISTA'
     ACTIVADO = 'ACTIVADO'
     CONTRA = 'CONTRA'
     OTROS = 'OTROS'
-    TIPO_CLIENTE = [
+    TIPO_PERSONA = [
         (ACTIVISTA, 'ACTIVISTA'),
         (ACTIVADO, 'ACTIVADO'),
         (CONTRA, "CONTRA"),
@@ -61,14 +78,26 @@ class Persona(models.Model):
     ]
 
     tipo = models.CharField(
-        max_length = 15,
-        choices = TIPO_CLIENTE,
-        default = ACTIVISTA
+        max_length=15,
+        choices=TIPO_PERSONA,
+        default=ACTIVISTA
+    )
+    
+    M = 'MASCULINO'
+    F = 'FEMENINO'
+    GENERO = [
+        (M, 'MASCULINO'),
+        (F, 'FEMENINO')
+    ]
+    genero = models.CharField(
+        max_length=15,
+        choices=GENERO,
+        default=M
     )
 
     domicilio = models.CharField(
-        max_length=150, 
-        blank = True
+        max_length=150,
+        blank=True
     )
 
     ACT = 'ACTIVO'
@@ -79,14 +108,14 @@ class Persona(models.Model):
     ]
 
     estado = models.CharField(
-        max_length = 10,
-        choices = ESTADO,
-        default = ACT
+        max_length=10,
+        choices=ESTADO,
+        default=ACT
     )
 
     telefono = models.BigIntegerField(
-        null = True,
-        blank = True
+        null=True,
+        blank=True
     )
 
     # llaves foráneas
@@ -102,34 +131,118 @@ class Persona(models.Model):
     def save(self):
         """ el nombre de la persona lo guardamos en mayúsculas"""
         self.nombres = self.nombres.upper()
-        # si la cadena esta llena, convertimos el contenido a mayúsculas
-        if self.ap_paterno or self.ap_materno:
+        if self.ap_paterno: # si la cadena esta llena
             self.ap_paterno = self.ap_paterno.upper()
+            
+        if self.ap_materno: # si la cadena esta llena
             self.ap_materno = self.ap_materno.upper()
-
+        
         super(Persona, self).save()
 
     class Meta:
         """ nombre en plural(Muchos) del modelo"""
         verbose_name_plural = "Personas"
-        
+
+
 class Departamento(models.Model):
     """ modelo departamento """
     nombre = models.CharField(max_length=100)
     numero_de_empleados = models.IntegerField(
-        blank = True,
-        null = True
-    )    
-    
+        blank=True,
+        null=True
+    )
+
+    # relaciones 1 o muchos
+    # un departamento tiene 1 o muchos empleados
+    #empleados = models.ManyToManyField(Empleado);
+
     def __str__(self):
         """ descripción del modelo departamento"""
         return '{}'.format(self.nombre)
-    
+
     def save(self):
         """ el nombre del departamento lo guardamos en mayúsculas"""
         self.nombre = self.nombre.upper()
         super(Departamento, self).save()
-             
+
+
+class Empleado(models.Model):
+    """ modelo empleado"""
+    nombre = models.CharField(max_length=100)
+    ap_paterno = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    ap_materno = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    domicilio = models.TextField(
+        max_length=250,
+        blank=True,
+        null=True
+    )
+
+    telefono = models.BigIntegerField(
+        blank=True,
+        null=True
+    )
+
+    M = 'MASCULINO'
+    F = 'FEMENINO'
+    GENERO = [
+        (M, 'MASCULINO'),
+        (F, 'FEMENINO')
+    ]
+    genero = models.CharField(
+        max_length=15,
+        choices=GENERO,
+        default=M
+    )
+
+    ACT = 'ACTIVO'
+    INA = 'INACTIVO'
+    ESTADO = [
+        (ACT, 'ACTIVO'),
+        (INA, 'INACTIVO')
+    ]
+
+    status = models.CharField(
+        max_length=10,
+        choices=ESTADO,
+        default=ACT
+    )
+
+    # relaciones
+    # un empleado trabaja en un departamento
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    # un empleado tiene asignado un puesto
+    puesto = models.ForeignKey(Puesto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """ descripción de empleado"""
+        return '{} {} {}'.format(self.nombre, self.ap_paterno, self.ap_materno)
+
+    def save(self):
+        """ el nombre del empleado lo guardamos en mayúsculas"""
+        self.nombre = self.nombre.upper()
+        if self.ap_paterno: # si la cadena esta llena
+            self.ap_paterno = self.ap_paterno.upper()
+            
+        if self.ap_materno:
+            self.ap_materno = self.ap_materno.upper()
+
+        super(Empleado, self).save()
+
+    class Meta:
+        """ nombre en plural(Muchos) del modelo"""
+        verbose_name_plural = "Empleados"
+
+
 class EncargadoRuta(models.Model):
     """ modelo para el encargado de ruta"""
     nombres = models.CharField(
@@ -137,11 +250,15 @@ class EncargadoRuta(models.Model):
     )
 
     ap_paterno = models.CharField(
-        max_length=100
+        max_length=100,
+        blank = True,
+        null = True
     )
 
     ap_materno = models.CharField(
-        max_length=100
+        max_length=100,
+        blank = True,
+        null = True
     )
 
     # choices
@@ -153,31 +270,31 @@ class EncargadoRuta(models.Model):
     ]
 
     sexo = models.CharField(
-        max_length = 10,
-        choices = TIPO_DE_SEXO,
-        default = HOMBRE
+        max_length=10,
+        choices=TIPO_DE_SEXO,
+        default=HOMBRE
     )
 
     telefono = models.BigIntegerField(
-        blank = True,
+        blank=True,
         null=True
     )
 
     domicilio = models.TextField(
         max_length=400,
-        blank = True
+        blank=True
     )
 
     email = models.EmailField(
-        null = True,
-        blank = True
+        null=True,
+        blank=True
     )
 
     # llaves foráneas
     # un encargado de ruta administra/controla una o muchas localidades
     comunidades = models.ManyToManyField(Localidad)
     # un encargado de ruta dirige un departamento
-    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE);
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
 
     def __str__(self):
         """ Descripción del modelo encargado de ruta """
@@ -186,16 +303,18 @@ class EncargadoRuta(models.Model):
     def save(self):
         """ el nombre del encargado de ruta lo guardamos en mayúsculas"""
         self.nombres = self.nombres.upper()
-        # si la cadena esta llena, convertimos el contenido a mayúsculas
-        if self.ap_paterno or self.ap_materno:
+        if self.ap_paterno: # si la cadena esta llena
             self.ap_paterno = self.ap_paterno.upper()
+            
+        if self.ap_materno: # si la cadena esta llena
             self.ap_materno = self.ap_materno.upper()
+            
         super(EncargadoRuta, self).save()
 
     class Meta:
         """ nombre en plural(Muchos) del modelo"""
         verbose_name_plural = "Encargados de Ruta"
-
+        
 class Apoyos(models.Model):
     """ modelo de apoyos"""
     # choices
@@ -217,32 +336,34 @@ class Apoyos(models.Model):
     ]
 
     tipo = models.CharField(
-        max_length = 20,
-        choices = TIPO_DE_APOYO,
-        default = ECONOMICO
+        max_length=20,
+        choices=TIPO_DE_APOYO,
+        default=ECONOMICO
     )
 
     descripcion = models.TextField(
-        max_length = 250
+        max_length=250
     )
 
     fecha_de_entrega = models.DateField(
         null=True, blank=True
     )
 
-    comprobacion = models.ImageField(upload_to="apoyos/", null=True, blank=True)
+    comprobacion = models.ImageField(
+        upload_to="apoyos/", null=True, blank=True)
 
     notas_adicionales = models.TextField(
-        max_length = 500,
-        null = True,
-        blank = True
+        max_length=500,
+        null=True,
+        blank=True
     )
 
     # llave foránea
     # un apoyo es entregado a una persona
     persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
     # un apoyo es entregado por un encargado
-    encargado_de_ruta = models.ForeignKey(EncargadoRuta, on_delete=models.CASCADE)
+    encargado_de_ruta = models.ForeignKey(
+        EncargadoRuta, on_delete=models.CASCADE)
     #persona_que_entrego = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
@@ -252,95 +373,3 @@ class Apoyos(models.Model):
     class Meta:
         """ nombre en plural(Muchos) del modelo"""
         verbose_name_plural = "Apoyos"
-        
-class Puesto(models.Model):
-    """ modelo de Puesto de trabajo"""
-    nombre = models.CharField(max_length=200)
-    
-    def __str__(self):
-        """ descripcion del puesto"""
-        return '{}'.format(self.nombre)
-    
-    def save(self):
-       """ el nombre del puesto de trabajo lo guardamos en mayúsculas"""
-       self.nombre = self.nombre.upper()
-       super(Puesto, self).save()
-       
-    class Meta:
-        """ nombre en plural(Muchos) del modelo"""
-        verbose_name_plural = "Puestos"
-        
-class Empleado(models.Model):
-    """ modelo empleado"""
-    nombre = models.CharField(max_length=100)
-    ap_paterno = models.CharField(
-        max_length = 100,
-        blank = True,
-        null =  True
-    )
-    
-    ap_materno = models.CharField(
-        max_length = 100,
-        blank = True,
-        null =  True
-    )
-    
-    domicilio = models.TextField(
-        max_length = 250,
-        blank = True,
-        null = True
-    )
-    
-    telefono = models.BigIntegerField(
-        blank = True,
-        null = True
-    )
-    
-    M = 'MASCULINO'
-    F = 'FEMENINO'
-    SEXO = [
-        ( M, 'MASCULINO'),
-        (F, 'FEMENINO')
-    ]
-    sexo = models.CharField(
-        max_length = 15,
-        choices = SEXO,
-        default = M
-    )
-    
-    ACT = 'ACTIVO'
-    INA = 'INACTIVO'
-    ESTADO = [
-        (ACT, 'ACTIVO'),
-        (INA, 'INACTIVO')
-    ]
-
-    status = models.CharField(
-        max_length = 10,
-        choices = ESTADO,
-        default = ACT
-    )
-    
-    #relaciones
-    # un empleado trabaja en un departamento
-    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    # un empleado tiene asignado un puesto
-    puesto = models.ForeignKey(Puesto, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        """ descripción de empleado"""
-        return '{} {} {}'.format(self.nombre, self.ap_paterno, self.ap_materno)
-    
-    def save(self):
-        """ el nombre del empleado lo guardamos en mayúsculas"""
-        self.nombre = self.nombre.upper()
-        # si la cadena esta llena, convertimos el contenido a mayúsculas
-        if self.ap_paterno or self.ap_materno: # si hay contenido en estos campos
-            self.ap_paterno = self.ap_paterno.upper()
-            self.ap_materno = self.ap_materno.upper()
-
-        super(Empleado, self).save()
-        
-    class Meta:
-        """ nombre en plural(Muchos) del modelo"""
-        verbose_name_plural = "Empleados"
