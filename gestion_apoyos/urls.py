39fbd772fb758f008a8fedc47a44740b91da22c0
Manuel Herrera Lara <m.herreralara80@gmail.com>
2022-11-24 03:07:02 -0600

    agregamos datatables al modulo de admin de personal

--- gestion_apoyos/urls.py
+++ gestion_apoyos/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
